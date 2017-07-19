4c29fad5e6e81d3f5d39f1b1148330885008f8a2
Arkadiusz Klein <arek.klein@limebrains.com>
2017-07-19 13:17:08 +0200

    full list of links for all pages

--- olx.py
+++ olx.py
@@ -1,4 +1,5 @@
 # python modules
+import re
 from urllib.parse import urlparse
 
 # third party
@@ -15,24 +16,41 @@ WHITELISTED_DOMAINS = [
 ]
 
 
-def get_url(main_category, subcategory, detail_category, region, **filters):
-    return "/".join([BASE_URL, main_category, subcategory, detail_category, region])
+def flatten(container):
+    for i in container:
+        if isinstance(i, (list, tuple)):
+            for j in flatten(i):
+                yield j
+        else:
+            yield i
+
+
+def get_url(main_category, subcategory, detail_category, region, page, **filters):
+    return "/".join([BASE_URL, main_category, subcategory, detail_category, region, page or ""])
 
 
 def get_content_for_url(url):
-    return requests.get(url).content
+    response = requests.get(url, allow_redirects=False)
+    try:
+        response.raise_for_status()
+    except requests.HTTPError as e:
+        print('Request for {} failed.'.format(url))
+        return None
+    return response
 
 
 def parse_offer(offer_markup):
     html_parser = BeautifulSoup(offer_markup, "html.parser")
     url = html_parser.find(class_="linkWithHash").attrs['href']
+
     if not url:
         # detail url is not present
-        return {}
+        return []
+
     if urlparse(url).hostname not in WHITELISTED_DOMAINS:
         # domain is not supported by this backend
-        return {}
-
+        return []
+    # print(urlparse(url).hostname, url)
     return {
         'detail_url': url
     }
@@ -42,15 +60,31 @@ def parse_content(markup):
     html_parser = BeautifulSoup(markup, "html.parser")
     offers = html_parser.find_all(class_='offer')
     parsed_offers = [parse_offer(str(offer)) for offer in offers if offer]
-    print(parsed_offers)
+    # delete featured offers (always first 3)
+    for i in range(3):
+        del parsed_offers[0]
+    return parsed_offers
 
 
 def get_category(main_category, subcategory, detail_category, region, **filters):
-    url = get_url(main_category, subcategory, detail_category, region, **filters)
-    content = get_content_for_url(url)
-    parsed_content = parse_content(content)
-    return content
+    parsed_content = []
+    page = 1
+    page_attr = None
+    while True:
+        url = get_url(main_category, subcategory, detail_category, region, page_attr, **filters)
+        response = get_content_for_url(url)
+        print(url)
+        if response.status_code > 300:
+            break
+        parsed_content.append(parse_content(response.content))
+        page += 1
+        page_attr = "?page={}".format(page)
+
+    parsed_content = list(flatten(parsed_content))
+    print(len(parsed_content))
+    print(parsed_content)
+    return parsed_content
 
 
 if __name__ == '__main__':
-    get_category("nieruchomosci", "mieszkania", "wynajem", "gdansk")
+    parsed_urls = get_category("nieruchomosci", "mieszkania", "wynajem", "gdansk")
