1e076259801d2ba190c5870a88ce1e26498283e9
Arkadiusz Klein <arek.klein@limebrains.com>
2017-07-20 14:24:15 +0200

    changed .contents for .text. added parsing for description, offer attributes and date

--- olx.py
+++ olx.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 from scrapper_helpers.utils import caching
 
 BASE_URL = 'https://www.olx.pl'
+OFFERS_FEATURED_PER_PAGE = 3
 DEBUG = True
 
 WHITELISTED_DOMAINS = [
@@ -41,7 +42,7 @@ def get_content_for_url(url):
     return response
 
 
-def parse_offer(offer_markup):
+def parse_offer_url(offer_markup):
     html_parser = BeautifulSoup(offer_markup, "html.parser")
     url = html_parser.find(class_="linkWithHash").attrs['href']
     if not url:
@@ -51,27 +52,43 @@ def parse_offer(offer_markup):
         # domain is not supported by this backend
         return []
     return url
-    # return {
-    #     'detail_url': url
-    # }
 
 
 def get_title(offer_markup):
     html_parser = BeautifulSoup(offer_markup, "html.parser")
-    title = html_parser.h1.contents
-    return title[0].replace("\n", "").replace("  ", "")
+    title = html_parser.h1.text
+    return title.replace("\n", "").replace("  ", "")
 
 
 def get_price(offer_markup):
     html_parser = BeautifulSoup(offer_markup, "html.parser")
-    price = html_parser.find(class_="xxxx-large").contents
-    return price[0]
+    price = html_parser.find(class_="xxxx-large").text
+    return price
+
+
+def get_yardage(offer_markup):
+    html_parser = BeautifulSoup(offer_markup, "html.parser")
+    yardage = html_parser.sup.parent.text
+    try:
+        return int(yardage.replace("\t", "").replace("\n", "").replace(" m", ""))
+    except ValueError:
+        return None
+
+
+def parse_description(offer_markup):
+    html_parser = BeautifulSoup(offer_markup, "html.parser")
+    description = html_parser.find(class_="large").text
+    return description.replace("  ", "")
 
 
 def get_img_url(offer_markup):
     html_parser = BeautifulSoup(offer_markup, "html.parser")
-    img = html_parser.find(class_="bigImage").attrs["src"]
-    return img
+    img = html_parser.find_all(class_="bigImage")
+    output = []
+    for element in img:
+        output.append(element.attrs["src"])
+
+    return output
 
 
 def get_date_added(offer_markup):
@@ -83,29 +100,73 @@ def get_date_added(offer_markup):
         return date[0].replace("Dodane", "").replace("\n", "").replace("  ", "").replace("o ", "").replace(", ", " ")
 
 
+# parses data from google tag manager script
+def parse_data(offer_markup):
+    html_parser = BeautifulSoup(offer_markup, "html.parser")
+    data = html_parser.find_all('script')
+    output = {"private_business": None, "floor": None, "rooms": None, "furniture": None, "builttype": None}
+    for element in data:
+        if element.string is not None:
+            current = element.string.split()
+            for ele in current:
+                if "private_business" in ele:
+                    tmp = ele.split(",")
+                    for i, value in enumerate(tmp):
+                        if "private_business" in value and "google" not in value:
+                            output["private_business"] = value.split('"')[3]
+                        elif "floor_select" in value:
+                            floor = value.split('"')
+                            floor_number = ""
+                            for char in floor[3]:
+                                if char.isdigit():
+                                    floor_number += char
+                            output["floor"] = int(floor_number)
+                        elif "rooms" in value:
+                            translate = {"one": 1, "two": 2, "three": 3, "four": 4}
+                            output["rooms"] = translate[value.split('"')[3]]
+                        elif "builttype" in value:
+                            output["builttype"] = value.split('"')[3]
+                        elif "furniture" in value:
+                            if value.split('"')[3] == "yes":
+                                output["furniture"] = True
+                            else:
+                                output["furniture"] = False
+    return output
+
+
 def parse_avalible_offers(markup):
     html_parser = BeautifulSoup(markup, "html.parser")
     offers = html_parser.find_all(class_='offer')
-    parsed_offers = [parse_offer(str(offer)) for offer in offers if offer]
-    # delete featured offers (always first 3)
-    for i in range(3):
-        del parsed_offers[0]
+    parsed_offers = [parse_offer_url(str(offer)) for offer in offers if offer][OFFERS_FEATURED_PER_PAGE:]
     return parsed_offers
 
 
-def parse_description(markup, url):
+def parse_offer(markup, url):
     html_parser = BeautifulSoup(markup, "html.parser")
-    offer_content = html_parser.find_all(class_='offerbody')
-    parsed_titles = [get_title(str(desc)) for desc in offer_content if desc]
-    parsed_prices = [get_price(str(desc)) for desc in offer_content if desc]
-    parsed_imgs = [get_img_url(str(desc)) for desc in offer_content if desc]
-    parsed_dates = [get_date_added(str(desc)) for desc in offer_content if desc]
+    offer_content = html_parser.find(class_='offerbody')
+    parsed_title = get_title(str(offer_content))
+    parsed_price = get_price(str(offer_content))
+    parsed_imgs = get_img_url(str(offer_content))
+    parsed_date = get_date_added(str(offer_content))
+    parsed_yardage = get_yardage(str(offer_content))
+    description = parse_description(str(offer_content))
+    offer_content = html_parser.body
+    offer_data = parse_data(str(offer_content))
+    keys = list(offer_data.keys())
+    values = list(offer_data.values())
     return {
-        "title": parsed_titles[0],
-        "price": parsed_prices[0],
+        "title": parsed_title,
+        "price": parsed_price,
+        "yardage": parsed_yardage,
+        keys[0]: values[0],
+        keys[1]: values[1],
+        keys[2]: values[2],
+        keys[3]: values[3],
+        keys[4]: values[4],
+        "description": description,
         "url": url,
-        "date": parsed_dates[0],
-        "img": parsed_imgs[0]
+        "date": parsed_date,
+        "images": parsed_imgs
     }
 
 
@@ -124,7 +185,7 @@ def get_category(main_category, subcategory, detail_category, region, **filters)
         page_attr = "?page={}".format(page)
 
     parsed_content = list(flatten(parsed_content))
-    print(str(len(parsed_content)) + "offers")
+    print(str(len(parsed_content)) + " offers")
     # print(parsed_content)
     return parsed_content
 
@@ -134,10 +195,11 @@ def get_description(parsed_urls):
     descriptions = []
     for url in parsed_urls:
         response = get_content_for_url(url)
-        descriptions.append(parse_description(response.content, url))
-        i += 1
-        if i > 5:
-            break
+        descriptions.append(parse_offer(response.content, url))
+        # if DEBUG:
+        #     i += 1
+        #     if i > 5:
+        #         break
     return descriptions
 
 
@@ -145,4 +207,5 @@ if __name__ == '__main__':
     parsed_urls = get_category("nieruchomosci", "mieszkania", "wynajem", "gdansk")
     descriptions = get_description(parsed_urls)
     for element in descriptions:
+        print()
         print(element)
