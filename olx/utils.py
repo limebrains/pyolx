5bcf66f6d3940fd5bc8520eac5b9c17b6a533ef2
Arkadiusz Klein <arek.klein@limebrains.com>
2017-08-09 13:20:50 +0200

    Remove random user-agent redefinition

--- olx/utils.py
+++ olx/utils.py
@@ -19,25 +19,6 @@ POLISH_CHARACTERS_MAPPING = {"ą": "a", "ć": "c", "ę": "e", "ł": "l", "ń": "
 
 log = logging.getLogger(__file__)
 
-USER_AGENTS = [
-    'Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36',
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-    '42.0.2311.135 Safari/537.36 Edge/12.246',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/'
-    '9.0.2 Safari/601.3.9',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
-]
-
-
-def get_random_user_agent():
-    """ Randoms user agent to prevent "python" user agent
-
-    :return: Random user agent from USER_AGENTS
-    :rtype: str
-    """
-    return random.choice(USER_AGENTS)
-
 
 def city_name(city):
     """ Creates valid OLX url city name
