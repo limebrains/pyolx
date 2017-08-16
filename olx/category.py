c488e56710260d6b3803641260dbf8c70e41f446
Arkadiusz Klein <arek.klein@gmail.com>
2017-08-16 16:28:17 +0200

    Fix for celery category backend (#13)

--- olx/category.py
+++ olx/category.py
@@ -36,22 +36,29 @@ def get_page_count(markup):
     return 1
 
 
-def get_page_count_for_filters(main_category, sub_category, detail_category, region, **filters):
+def get_page_count_for_filters(main_category=None, sub_category=None, detail_category=None, region=None,
+                               search_query=None, url=None, **filters):
     """ Reads total page number for given search filters
 
+    :param url: User defined url for OLX page with offers. It overrides category parameters and applies search filters.
     :param main_category: Main category
     :param sub_category: Sub category
     :param detail_category: Detail category
     :param region: Region of search
+    :param search_query: Additional search query
     :param filters: See :meth category.get_category for reference
-    :type main_category: str
-    :type sub_category: str
-    :type detail_category: str
-    :type region: str
+    :type url: str, None
+    :type main_category: str, None
+    :type sub_category: str, None
+    :type detail_category: str, None
+    :type region: str, None
+    :type search_query: str, None
     :return: Total page number
     :rtype: int
     """
-    url = get_url(main_category, sub_category, detail_category, region, **filters)
+    city = city_name(region) if region else None
+    if url is None:
+        url = get_url(main_category, sub_category, detail_category, city, search_query, **filters)
     response = get_content_for_url(url)
     html_parser = BeautifulSoup(response.content, "html.parser")
     script = html_parser.head.script.next_sibling.next_sibling.next_sibling.text.split(",")
@@ -156,12 +163,12 @@ def get_category(main_category=None, sub_category=None, detail_category=None, re
         "[filter_enum_rooms][0]": 2 # desired number of rooms, enum: from 1 to 4 (4 and more)
     }
 
-    :type url: str
-    :type main_category: str
-    :type sub_category: str
-    :type detail_category: str
-    :type region: str
-    :type search_query: str
+    :type url: str, None
+    :type main_category: str, None
+    :type sub_category: str, None
+    :type detail_category: str, None
+    :type region: str, None
+    :type search_query: str, None
     :type filters: dict
     :return: List of all offers for given parameters
     :rtype: list
@@ -192,26 +199,33 @@ def get_category(main_category=None, sub_category=None, detail_category=None, re
     return parsed_content
 
 
-def get_offers_for_page(main_category, sub_category, detail_category, region, page, **filters):
+def get_offers_for_page(page, main_category=None, sub_category=None, detail_category=None, region=None,
+                        search_query=None, url=None, **filters):
     """ Parses offers for one specific page of given category with filters.
 
+    :param page: Page number
+    :param url: User defined url for OLX page with offers. It overrides category parameters and applies search filters.
     :param main_category: Main category
     :param sub_category: Sub category
     :param detail_category: Detail category
     :param region: Region of search
-    :param page: Page number
     :param filters: See :meth category.get_category for reference
-    :type main_category: str
-    :type sub_category: str
-    :type detail_category: str
-    :type region: str
     :type page: int
+    :type url: str, None
+    :type main_category: str, None
+    :type sub_category: str, None
+    :type detail_category: str, None
+    :type region: str, None
+    :type search_query: str, None
     :type filters: dict
     :return: List of all offers for given page and parameters
     :rtype: list
     """
-    city = city_name(region)
-    url = get_url(main_category, sub_category, detail_category, city, page=page, **filters)
+    city = city_name(region) if region else None
+    if url is None:
+        url = get_url(main_category, sub_category, detail_category, city, search_query, page=page, **filters)
+    else:
+        url = get_url(page=page, user_url=url, **filters)
     response = get_content_for_url(url)
     log.info("Loaded page {0} of offers".format(page))
     offers = parse_available_offers(response.content)
